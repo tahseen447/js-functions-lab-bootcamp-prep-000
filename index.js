47f3cfef7e6052074a50daeafbd92137be8410bc
tahseen447 <tahseenfatima1986@gmail.com>
2017-09-29 08:46:19 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -11,7 +11,7 @@ function happyHolidaysTo(name)
 
 function happyHolidayTo(holiday, name)
 {
-  return '${holiday}, ${name}!'
+  return 'Happy ${holiday}, ${name}!'
 }
 function holidayCountdown(holiday, days)
 {
