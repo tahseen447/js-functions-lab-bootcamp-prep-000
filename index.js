9f01559e53e86b89e1181e9ad112d94bb4a37100
tahseen447 <tahseenfatima1986@gmail.com>
2017-09-29 08:45:34 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -9,7 +9,7 @@ function happyHolidaysTo(name)
   return "Happy holidays, ${name}!"
 }
 
-function happyHolidaysTo(holiday, name)
+function happyHolidayTo(holiday, name)
 {
   return '${holiday}, ${name}!'
 }
